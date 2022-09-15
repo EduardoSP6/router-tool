9e951eebc5fe46814b968e0cbd93fe429a684d95
Eduardo Sales <eduardo.sp6@gmail.com>
2022-09-15 16:36:17 -0300

    publish config file name fixed; remove package discover from composer.json; require php >=7.4 and illuminate/support >= 7.x

--- src/RouterToolServiceProvider.php
+++ src/RouterToolServiceProvider.php
@@ -9,12 +9,16 @@ class RouterToolServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../config/pages.php' => config_path('router-tool.php'),
+            __DIR__ . '/../config/router-tool.php' => config_path('router-tool.php'),
         ], 'config');
     }
 
     public function register()
     {
+        $this->app->singleton(RouterTool::class, function($app) {
+            return new RouterTool();
+        });
+
         $this->mergeConfigFrom(
             __DIR__ . '/../config/router-tool.php',
             'router-tool'
